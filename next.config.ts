8e9487d455b3a2d1ffe9d88eae28f95974c37d32
Raphael Wisdom Chidera <heisdera@gmail.com>
2025-05-23 04:42:22 +0100

    refactor: update next.config.ts and add droip metadata title, description and favicon links

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
+const isDev = process.env.NODE_ENV === "development"
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*",
+      },
+    ],
+  },
+  compiler: {
+    removeConsole: !isDev,
+  },
 };
 
 export default nextConfig;
